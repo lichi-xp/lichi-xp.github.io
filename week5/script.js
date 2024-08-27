e988566520b0d9efddf2d38fb237c688b7222675
lichi-xp <xuanphuc26102004@gmail.com>
2024-08-27 10:48:08 +1000

    update script.js

--- week5/script.js
+++ week5/script.js
@@ -1,1 +1,24 @@
 console.log("welcome to the class");
+let myName = "alice";
+const myCity02 = "melbourne";
+console.log(myName, "lives in", myCity02);
+
+let count = 10;
+console.log(count);
+
+let isItMorningClass = true;
+let isItAfternoonClass = false;
+
+let myStudentRecord = {
+  name: "XPhuc",
+  id: 1234,
+  class: "OART1013",
+  isItScience: false,
+  isItDesign: true,
+};
+
+console.log("Hello everyone, my name is", myStudentRecord.name);
+
+console.log("my id is", myStudentRecord.id);
+console.log("my class is", myStudentRecord.class);
+console.log("are you science student?", myStudentRecord.isItScience);
